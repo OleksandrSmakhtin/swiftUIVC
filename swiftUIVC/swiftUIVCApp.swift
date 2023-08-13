4816205f9f472df12c32f76c3e9cd5b19419635e
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-08-13 09:59:00 +0100

    Initial Commit

---
+++ swiftUIVC/swiftUIVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIVCApp.swift
+//  swiftUIVC
+//
+//  Created by Oleksandr Smakhtin on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
